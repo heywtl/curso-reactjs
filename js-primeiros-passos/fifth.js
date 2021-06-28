cdd66f0103c63bbb966de14f4bcb4bdb037845cc
Lincoln Souza <lincolnams4@gmail.com>
2021-06-28 13:09:02 -0300

    second lesson files

---
+++ js-primeiros-passos/fifth.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 22;
+const primeiroNome = "Lincoln";
+const sobrenome = "Souza";
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(primeiroNome);
---
+++ js-primeiros-passos/second.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com Variáveis");
+//JS é case sensitive
+
+const idade = 22;
+const nome = "Lincoln";
+
+//não declarar variáveis como
+//nome = "Lincoln";
+
+console.log(idade);
